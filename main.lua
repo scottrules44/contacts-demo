84387033836c7778f47c106f2c14f4007839c380
Scott Harrrison2 <scottharrrison2@Scotts-MacBook-Pro-5.local>
2016-11-30 16:15:19 -0600

    1.2
    bug fixes

--- main.lua
+++ main.lua
@@ -1,29 +1,91 @@
-local contacts = require "plugin.contacts"
-local json = require "json"
-local bg = display.newRect( display.contentCenterX, display.contentCenterY, display.actualContentWidth, display.actualContentHeight )
-bg:setFillColor( 1 )
-local title = display.newText( "Contacts", 0-30, display.contentCenterY , native.systemFont )
-local getButton = display.newText( "Get Contacts",display.contentCenterX, display.contentCenterY-100, native.systemFont )
-getButton:setFillColor(0)
-getButton:addEventListener( "tap", function (  )
-	print( "Contacts" )
-	print( "----" )
-	contacts.get(function(e)
-    	print(json.prettify(e))
-	end)
-	print( "----" )
-end )
+local contacts = require ("plugin.contacts")
+local json = require ("json")
+local widget = require("widget")
 
-local accessButton = display.newText( "Can Access Contacts?",display.contentCenterX, display.contentCenterY, native.systemFont )
-accessButton:setFillColor(0)
-accessButton:addEventListener( "tap", function (  )
-	print(contacts.canAccess())
-end )
-local requestButton = display.newText( "Request Access( iOS and Andorid 6.0+)",display.contentCenterX, display.contentCenterY+100, native.systemFont )
-requestButton:setFillColor(0)
-accessButton:addEventListener( "tap", function (  )
-	contacts.requestAccess(function(e)
-    	print(json.prettify(e))
-	end)
-end )
 
+local background = display.newRect(display.contentWidth * 0.5, display.contentHeight * 0.5, display.contentWidth, display.contentHeight * 1.2 )
+
+    local background_gradient = {
+        type="gradient",
+        color1={ 1, 1, 1 }, color2={ 0.6, 0.6, 0.6 }, direction="down"
+    }
+    background:setFillColor( background_gradient )
+    
+
+    local function Button1Event( event )
+
+        if ( "ended" == event.phase ) then
+            contacts.get(function(event)
+            print(json.prettify(event))
+             --native.showAlert( "Status", tostring(event.status) , { "OK" })
+                if event.status == "success" then
+                    --native.showAlert( "Contacts", tostring(event.contacts) , { "OK" })
+                end
+            end)
+  
+        end
+    end
+
+    
+    local Button1 = widget.newButton(
+    {
+        width = display.contentWidth * 0.8,
+        height = display.contentHeight * 0.1,
+        label = "Get Contracts",
+        labelColor = { default={ 0,0,0 }, over={ 1,1,1, 0.5 } },
+        onEvent = Button1Event
+    }
+)
+    Button1.x = display.contentWidth * 0.5
+    Button1.y = display.contentHeight * 0.1
+    
+    
+    
+    
+    
+     local function Button2Event( event )
+
+        if ( "ended" == event.phase ) then
+            print(contacts.canAccess())
+            native.showAlert( "Can Access", contacts.canAccess(), { "OK" })
+        end
+    end
+
+    
+    local Button2 = widget.newButton(
+    {
+        width = display.contentWidth * 0.8,
+        height = display.contentHeight * 0.1,
+        label = "Access Contacts?",
+        labelColor = { default={ 0,0,0 }, over={ 1,1,1, 0.5 } },
+        onEvent = Button2Event
+    }
+)
+    Button2.x = display.contentWidth * 0.5
+    Button2.y = display.contentHeight * 0.3
+    
+    
+    
+    
+     local function Button3Event( event )
+
+        if ( "ended" == event.phase ) then
+           contacts.requestAccess(function(event)
+            print(json.prettify(event))
+                --native.showAlert( "Request Access", tostring(event.status) , { "OK" })  
+            end)
+        end
+    end
+
+    
+    local Button3 = widget.newButton(
+    {
+        width = display.contentWidth * 0.8,
+        height = display.contentHeight * 0.1,
+        label = "Request Access",
+        labelColor = { default={ 0,0,0 }, over={ 1,1,1, 0.5 } },
+        onEvent = Button3Event
+    }
+)
+    Button3.x = display.contentWidth * 0.5
+    Button3.y = display.contentHeight * 0.5
